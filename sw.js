ad70d00d1960e13db84c008005712c8350b44a20
Marcos Emanuel <marcosemanuelll20-creator@users.noreply.github.com>
2026-09-22 19:08:55 -0300

    fix: finalize church app permissions and sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'casa-do-oleiro-v2';
+const CACHE = 'casa-do-oleiro-v3';
 const ASSETS = ['./', './index.html', './logo-casa-do-oleiro.png', './manifest.webmanifest'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
